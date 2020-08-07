57a5cbe97170a7ae1c3d3b0e5b743e1bfdfe0fd9
7Osman7 <33115823+7Osman7@users.noreply.github.com>
2020-08-06 18:31:59 -0700

    Add files via upload
    Added config.py file

---
+++ config.py
@@ -0,0 +1,15 @@
+#This is where hyper-parameters and paths are edited
+
+
+
+#Hyper-parameters
+EPOCHS = 50
+BATCH_SIZE = 64
+LEARNING_RATE = 0.01
+
+#Paths
+TRAIN_PATH = 'C:\\Users\\MONB1\\Desktop\\Gluxkind_CNN\\training'
+VALID_PATH = 'C:\\Users\\MONB1\\Desktop\\Gluxkind_CNN\\validation'
+TEST_PATH = 'C:\\Users\\MONB1\\Desktop\\Gluxkind_CNN\\testing'
+MODEL_STORE_PATH = 'C:\\Users\\MONB1\\Desktop\\Gluxkind_CNN\\'
+
